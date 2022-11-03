4c698905fe36630323121fa158886382ce112417
Leo Ledlow <31348319+CascaTheGreat@users.noreply.github.com>
2022-11-03 19:03:07 +0000

    Added roughly 100000 new data points

---
+++ main.py
@@ -0,0 +1,9 @@
+import Classifier
+import os
+
+os.system(
+    "pip3 install git+https://github.com/JustAnotherArchivist/snscrape.git")
+while True:
+    user = input("User: ")
+    allign = input("Allignment: ")
+    Classifier.classify(user, "300", allign)

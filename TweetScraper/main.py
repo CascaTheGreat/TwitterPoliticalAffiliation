aa97e2265109bf39146968b8b42f38148579d951
Leo Ledlow <31348319+CascaTheGreat@users.noreply.github.com>
2022-11-03 14:08:28 -0500

    Delete TweetScraper directory

--- TweetScraper/main.py
+++
@@ -1,7 +0,0 @@
-import Classifier
-import os
-os.system("pip3 install git+https://github.com/JustAnotherArchivist/snscrape.git")
-while True:
-  user = input("User: ")
-  allign = input("Allignment: ")
-  Classifier.classify(user,"300",allign)
